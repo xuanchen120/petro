0eac33735e5d18b4e932a6277d7cb349c45cf92c
xuanchen <122383162@qq.com>
2022-07-21 16:07:59 +0800

    调整回调

--- src/Petro/config.php
+++ src/Petro/config.php
@@ -4,23 +4,25 @@ return [
     /**
      * 服务器地址
      */
-    'base_uri'      => env('PETRO_BASE_URI', ''),
+    'base_uri'       => env('PETRO_BASE_URI', ''),
     /**
      * 字符集
      */
-    'characterSet'  => '00',
+    'characterSet'   => '00',
     /**
      * 商户编号
      */
-    'strVendorCode' => env('PETRO_STR_VENDOR_CODE', ''),
+    'strVendorCode'  => env('PETRO_STR_VENDOR_CODE', ''),
     /**
      *商户秘钥
      */
-    'merchantKey'   => env('PETRO_MERCHANT_KEY', ''),
+    'merchantKey'    => env('PETRO_MERCHANT_KEY', ''),
     /**
      * 单个电子券秘钥
      */
-    'couponKey'     => env('PETRO_COUPON_KEY', ''),
-
-    'ysd_notice_url' => 'https://lifetest.ysd-bs.com/api/store/callback',
+    'couponKey'      => env('PETRO_COUPON_KEY', ''),
+    /**
+     * 回调通知地址
+     */
+    'ysd_notice_url' => env('PETRO_NOTICE_URL', ''),
 ];
